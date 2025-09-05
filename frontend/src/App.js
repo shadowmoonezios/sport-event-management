a1d74fb6ee467c2b6ac492a883277d2150d238ae
shadowmoonezios <agushifelde@hotmail.com>
2025-09-05 21:25:23 +0300

    feat: initial commit for frontend/src/App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sport Event Management</h1>
+      <p>Willkommen zur Sportveranstaltungsverwaltung!</p>
+    </div>
+  );
+}
+
+export default App;
