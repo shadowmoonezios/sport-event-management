c8dc80be0d43b9e4c250e7590ea788bf1694369d
shadowmoonezios <agushifelde@hotmail.com>
2025-09-05 21:25:22 +0300

    feat: initial commit for backend/models/User.js

---
+++ backend/models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  registeredEvents: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Event' }]
+});
+
+module.exports = mongoose.model('User', UserSchema);
